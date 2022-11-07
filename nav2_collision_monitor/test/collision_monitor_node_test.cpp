905aa1db2cdaa692cbc755fb3ed9a03d7694ccd5
PGotzmann <16465789+PGotzmann@users.noreply.github.com>
2022-11-07 10:54:04 +0100

    backport tests (lifecycle publisher activation)
    footprint related tests are allowed to fail since we currently assume "map" as reference frame

--- nav2_collision_monitor/test/collision_monitor_node_test.cpp
+++ nav2_collision_monitor/test/collision_monitor_node_test.cpp
@@ -123,6 +123,7 @@ public:
 
   // Configuring
   void setCommonParameters();
+  void activateTestPublishers();
   void addPolygon(
     const std::string & polygon_name, const PolygonType type,
     const double size, const std::string & at);
@@ -237,6 +238,15 @@ void Tester::setCommonParameters()
     "stop_pub_timeout", rclcpp::ParameterValue(STOP_PUB_TIMEOUT));
   cm_->set_parameter(
     rclcpp::Parameter("stop_pub_timeout", STOP_PUB_TIMEOUT));
+}
+
+void Tester::activateTestPublishers()
+{
+  std::dynamic_pointer_cast<rclcpp_lifecycle::LifecyclePublisherInterface>(footprint_pub_)->on_activate();
+  std::dynamic_pointer_cast<rclcpp_lifecycle::LifecyclePublisherInterface>(scan_pub_)->on_activate();
+  std::dynamic_pointer_cast<rclcpp_lifecycle::LifecyclePublisherInterface>(pointcloud_pub_)->on_activate();
+  std::dynamic_pointer_cast<rclcpp_lifecycle::LifecyclePublisherInterface>(range_pub_)->on_activate();
+  std::dynamic_pointer_cast<rclcpp_lifecycle::LifecyclePublisherInterface>(cmd_vel_in_pub_)->on_activate();
 }
 
 void Tester::addPolygon(
@@ -551,6 +561,7 @@ TEST_F(Tester, testProcessStopSlowdown)
   // Set Collision Monitor parameters.
   // Making two polygons: outer polygon for slowdown and inner for robot stop.
   setCommonParameters();
+  activateTestPublishers();
   addPolygon("SlowDown", POLYGON, 2.0, "slowdown");
   addPolygon("Stop", POLYGON, 1.0, "stop");
   addSource(SCAN_NAME, SCAN);
@@ -609,6 +620,7 @@ TEST_F(Tester, testProcessApproach)
   // Set Collision Monitor parameters.
   // Making one circle footprint for approach.
   setCommonParameters();
+  activateTestPublishers();
   addPolygon("Approach", CIRCLE, 1.0, "approach");
   addSource(SCAN_NAME, SCAN);
   setVectors({"Approach"}, {SCAN_NAME});
@@ -671,6 +683,7 @@ TEST_F(Tester, testProcessApproachRotation)
   // Set Collision Monitor parameters.
   // Making one circle footprint for approach.
   setCommonParameters();
+  activateTestPublishers();
   addPolygon("Approach", POLYGON, 1.0, "approach");
   addSource(RANGE_NAME, RANGE);
   setVectors({"Approach"}, {RANGE_NAME});
@@ -737,6 +750,7 @@ TEST_F(Tester, testCrossOver)
   // Making two polygons: outer polygon for slowdown and inner circle
   // as robot footprint for approach.
   setCommonParameters();
+  activateTestPublishers();
   addPolygon("SlowDown", POLYGON, 2.0, "slowdown");
   addPolygon("Approach", CIRCLE, 1.0, "approach");
   addSource(POINTCLOUD_NAME, POINTCLOUD);
@@ -793,6 +807,7 @@ TEST_F(Tester, testCeasePublishZeroVel)
 
   // Configure stop and approach zones, and basic data source
   setCommonParameters();
+  activateTestPublishers();
   addPolygon("Stop", POLYGON, 1.0, "stop");
   addPolygon("Approach", CIRCLE, 2.0, "approach");
   addSource(SCAN_NAME, SCAN);
@@ -854,6 +869,7 @@ TEST_F(Tester, testProcessNonActive)
   rclcpp::Time curr_time = cm_->now();
 
   setCommonParameters();
+  activateTestPublishers();
   addPolygon("Stop", POLYGON, 1.0, "stop");
   addSource(SCAN_NAME, SCAN);
   setVectors({"Stop"}, {SCAN_NAME});
@@ -876,6 +892,7 @@ TEST_F(Tester, testProcessNonActive)
 TEST_F(Tester, testIncorrectPolygonType)
 {
   setCommonParameters();
+  activateTestPublishers();
   addPolygon("UnknownShape", POLYGON_UNKNOWN, 1.0, "stop");
   addSource(SCAN_NAME, SCAN);
   setVectors({"UnknownShape"}, {SCAN_NAME});
@@ -887,6 +904,7 @@ TEST_F(Tester, testIncorrectPolygonType)
 TEST_F(Tester, testIncorrectSourceType)
 {
   setCommonParameters();
+  activateTestPublishers();
   addPolygon("Stop", POLYGON, 1.0, "stop");
   addSource("UnknownSource", SOURCE_UNKNOWN);
   setVectors({"Stop"}, {"UnknownSource"});
@@ -898,6 +916,7 @@ TEST_F(Tester, testIncorrectSourceType)
 TEST_F(Tester, testPolygonsNotSet)
 {
   setCommonParameters();
+  activateTestPublishers();
   addPolygon("Stop", POLYGON, 1.0, "stop");
   addSource(SCAN_NAME, SCAN);
 
@@ -908,6 +927,7 @@ TEST_F(Tester, testPolygonsNotSet)
 TEST_F(Tester, testSourcesNotSet)
 {
   setCommonParameters();
+  activateTestPublishers();
   addPolygon("Stop", POLYGON, 1.0, "stop");
   addSource(SCAN_NAME, SCAN);
   cm_->declare_parameter("polygons", rclcpp::ParameterValue({"Stop"}));
