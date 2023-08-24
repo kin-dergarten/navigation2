689c127d7a9071d9a7f094707ae81a5a26f9b5df
PGotzmann <16465789+PGotzmann@users.noreply.github.com>
2023-08-24 13:49:00 +0200

    added additional metrics for external localization monitoring (#9)
    * add localization metrics message
    
    * publish max weight as localization metric
    
    * added total sensor model score
    
    as a metric for how well the scan matches the map in the current estimate

--- nav2_amcl/include/nav2_amcl/amcl_node.hpp
+++ nav2_amcl/include/nav2_amcl/amcl_node.hpp
@@ -35,6 +35,7 @@
 #include "nav2_amcl/sensors/laser/laser.hpp"
 #include "nav2_msgs/msg/particle.hpp"
 #include "nav2_msgs/msg/particle_cloud.hpp"
+#include "nav2_msgs/msg/localization_metrics.hpp"
 #include "nav_msgs/srv/set_map.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "std_srvs/srv/empty.hpp"
@@ -170,6 +171,8 @@ protected:
     initial_pose_sub_;
   rclcpp_lifecycle::LifecyclePublisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr
     pose_pub_;
+  rclcpp_lifecycle::LifecyclePublisher<nav2_msgs::msg::LocalizationMetrics>::SharedPtr
+    metrics_pub_;
   rclcpp_lifecycle::LifecyclePublisher<nav2_msgs::msg::ParticleCloud>::SharedPtr
     particle_cloud_pub_;
   /*
@@ -268,6 +271,7 @@ protected:
   pf_t * pf_{nullptr};
   bool pf_init_;
   pf_vector_t pf_odom_pose_;
+  double tolal_sensor_model_score_;
   int resample_count_{0};
 
   // Laser scan related
