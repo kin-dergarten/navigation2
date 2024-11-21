23cc6a0ca8d9434d590a229fdab63f782fb08366
schupf2 <katrin.schupfer@knapp.com>
2024-11-21 10:41:00 +0100

    Only publish the costmap if it's current. Furthermore, fix `isCurrent` Logic to only report true if the costmap is really current (also after a reset).

--- nav2_costmap_2d/src/costmap_2d_ros.cpp
+++ nav2_costmap_2d/src/costmap_2d_ros.cpp
@@ -486,12 +486,17 @@ Costmap2DROS::mapUpdateLoop(double frequency)
         costmap_publisher_->updateBounds(x0, xn, y0, yn);
 
         auto current_time = now();
-        if ((last_publish_ + publish_cycle_ < current_time) ||  // publish_cycle_ is due
-          (current_time < last_publish_))      // time has moved backwards, probably due to a switch to sim_time // NOLINT
+        auto map_current = isCurrent();
+        if (map_current &&  // only update costmap if it's current
+          ((last_publish_ + publish_cycle_ < current_time) ||  // publish_cycle_ is due
+          (current_time < last_publish_)))     // time has moved backwards, probably due to a switch to sim_time // NOLINT
         {
           RCLCPP_DEBUG(get_logger(), "Publish costmap at %s", name_.c_str());
           costmap_publisher_->publishCostmap();
           last_publish_ = current_time;
+        }
+        else if (!map_current) {
+          RCLCPP_WARN(get_logger(), "Costmap was not published because it`s not current");
         }
       }
     }
