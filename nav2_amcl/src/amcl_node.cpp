aa8e771e43173c8dfcf7e4ef3550c992793e28b3
Michael HEINE <michael.heine@knapp.com>
2025-04-08 09:37:44 +0200

    Swapped branches of handling the initial pose in AMCL node

--- nav2_amcl/src/amcl_node.cpp
+++ nav2_amcl/src/amcl_node.cpp
@@ -281,7 +281,9 @@ AmclNode::on_activate(const rclcpp_lifecycle::State & /*state*/)
   // process incoming callbacks until we are
   active_ = true;
 
-  if (set_initial_pose_) {
+  if (init_pose_received_on_inactive) {
+    handleInitialPose(last_published_pose_);
+  } else if (set_initial_pose_) {
     auto msg = std::make_shared<geometry_msgs::msg::PoseWithCovarianceStamped>();
 
     msg->header.stamp = now();
@@ -292,8 +294,6 @@ AmclNode::on_activate(const rclcpp_lifecycle::State & /*state*/)
     msg->pose.pose.orientation = orientationAroundZAxis(initial_pose_yaw_);
 
     initialPoseReceived(msg);
-  } else if (init_pose_received_on_inactive) {
-    handleInitialPose(last_published_pose_);
   }
 
   auto node = shared_from_this();
