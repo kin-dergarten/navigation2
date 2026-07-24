ad4643a8fe88e411cd8dbd1bb0570b424fd77f50
avula <praneeth.avula@knapp.com>
2026-07-24 12:11:37 +0200

    Set action type as EMG_STOP and velocity to zero for immediate collisions in approach model

--- nav2_collision_monitor/src/collision_monitor_node.cpp
+++ nav2_collision_monitor/src/collision_monitor_node.cpp
@@ -527,6 +527,14 @@ bool CollisionMonitor::processApproach(
   // Obtain time before a collision
   const double collision_time = polygon->getCollisionTime(collision_points, velocity);
   if (collision_time >= 0.0) {
+    if (collision_time == 0.0) {
+      // Setting up zero velocity for immediate collision
+      robot_action.action_type = EMG_STOP;
+      robot_action.req_vel.x = 0.0;
+      robot_action.req_vel.y = 0.0;
+      robot_action.req_vel.tw = 0.0;
+      return true;
+    }
     // If collision will occurr, reduce robot speed
     const double change_ratio = collision_time / polygon->getTimeBeforeCollision();
     const Velocity safe_vel = velocity * change_ratio;
